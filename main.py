92b4ca15d3109dedd1a79d59e519f2fd24bf58b7
Karthik Ramesan <136253498+fsydurden@users.noreply.github.com>
2025-05-31 20:24:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+from random import choice
+
+from art import logo
+print(logo)
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+
+addition = add
+
+operations = {"+": add ,
+             "-": sub ,
+             "*": mul ,
+             "/": div ,}
+
+# print(operations["*"](4,8))
+
+num1 = int(input("Enter the first number: \t"))
+choice = str(input("What operation do you want to perform "
+                   "+ for addition\n"
+                   "- for substraction\n"
+                   "* for multiplication\n"
+                   "/ for division\n"
+                   "enter your choice \t"))
+num2 = int(input("Enter second number: \t"))
+
+def calculation(number1, number2, choices):
+    result = 0
+    if choices == "+":
+        result = operations["+"](number1, number2)
+    elif choices == "-":
+        result = operations["-"](number1, number2)
+    elif choices == "*":
+        result = operations["*"](number1, number2)
+    elif choices == "/":
+        result = operations["/"](number1, number2)
+    else:
+        print("Invalid choice")
+    # print(result)
+    return result
+
+working = True
+while working:
+    pre_result = calculation(num1, num2,choice)
+    og_result = calculation(num1, num2,choice)
+    print(og_result)
+    need = str(input("Press y if you want to continue with previous result or enter n for new calculation"))
+    if need == "y":
+        choice = str(input("What operation do you want to perform "
+                           "+ for addition\n"
+                           "- for substraction\n"
+                           "* for multiplication\n"
+                           "/ for division\n"
+                           "enter your choice \t"))
+        num2 = int(input("Enter second number: \t"))
+        calculation(pre_result, num2,choice)
+    elif need == "n":
+        print("\n" * 100)
