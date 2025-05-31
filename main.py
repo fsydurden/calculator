0ab61819d610499a339f5cfed8c21e6390504f8a
Karthik Ramesan <136253498+fsydurden@users.noreply.github.com>
2025-05-31 20:43:32 +0530

    fixed the wrong result usage

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from random import choice
-
 from art import logo
 print(logo)
 def add(n1, n2):
@@ -50,10 +48,10 @@ def calculation(number1, number2, choices):
 working = True
 while working:
     pre_result = calculation(num1, num2,choice)
-    og_result = calculation(num1, num2,choice)
-    print(og_result)
+    print(pre_result)
     need = str(input("Press y if you want to continue with previous result or enter n for new calculation"))
     if need == "y":
+        num1 = pre_result
         choice = str(input("What operation do you want to perform "
                            "+ for addition\n"
                            "- for substraction\n"
@@ -61,6 +59,20 @@ while working:
                            "/ for division\n"
                            "enter your choice \t"))
         num2 = int(input("Enter second number: \t"))
+
         calculation(pre_result, num2,choice)
     elif need == "n":
         print("\n" * 100)
+        num1 = int(input("Enter the first number: \t"))
+        choice = str(input("What operation do you want to perform "
+                           "+ for addition\n"
+                           "- for substraction\n"
+                           "* for multiplication\n"
+                           "/ for division\n"
+                           "enter your choice \t"))
+        num2 = int(input("Enter second number: \t"))
+        pre_result = calculation(num1, num2, choice)
+
+    else:
+        print("Invalid input. Exiting calculator.")
+        working = False
